dc4b4524c691731c3be8d2d0bd2381b07e7fb06b
Nihad Jabrayilzade <jabrayilzade.nihad@gmail.com>
2026-07-02 13:31:11 +0200

    feat: integrate Kafka configuration into FluidsModule

--- analysis/apps/fluids/src/FluidsModule.ts
+++ analysis/apps/fluids/src/FluidsModule.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { FluidsController } from "./FluidsController";
 import { FluidsService } from "./FluidsService";
+import { KAFKA_CONFIG } from "@shared";
+import { ClientsModule } from "@nestjs/microservices";
 
 @Module({
-  imports: [],
+  imports: [ClientsModule.register([KAFKA_CONFIG])],
   controllers: [FluidsController],
   providers: [FluidsService],
 })
